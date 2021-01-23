58e80c0438c9427f0690d89990d80ed77670a328
Ram Suthar <ramsuthar305@gmail.com>
2021-01-23 16:29:21 +0530

    main function bug fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from index_images import tree
 from search import search
-
+import os
 if __name__ == "__main__":
+    os.mkdir("pickle")
+    os.mkdir("output")
     treeObj = tree()
     treeObj.run()
     searchObj = search()
